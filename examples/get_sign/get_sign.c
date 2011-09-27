d151d06a875cbbaa0696e3c2d58158c2e2195995
Cristian Cadar <cristic@cs.stanford.edu>
2011-09-27 11:08:04 +0000

    Changed Tutorial 1, which was causing confusion due to differences in LLVM code generation before and after LLVM 2.8.
    git-svn-id: https://llvm.org/svn/llvm-project/klee/trunk@140602 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ examples/get_sign/get_sign.c
@@ -0,0 +1,20 @@
+/*
+ * First KLEE tutorial: testing a small function
+ */
+
+
+int get_sign(int x) {
+  if (x == 0)
+     return 0;
+  
+  if (x < 0)
+     return -1;
+  else 
+     return 1;
+} 
+
+int main() {
+  int a;
+  klee_make_symbolic(&a, sizeof(a), "a");
+  return get_sign(a);
+} 
