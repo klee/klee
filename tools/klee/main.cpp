1715ee37118cdf8785a1dd70d812b6a88ad623e7
Cristian Cadar <cristic@cs.stanford.edu>
2012-01-25 16:49:50 +0000

    Patch from Ben Gras which "makes Klee look for the libraries in the install location instead of the compile-tree location if a compile-time macro is enabled."
    git-svn-id: https://llvm.org/svn/llvm-project/klee/trunk@148961 91177308-0d34-0410-b5e6-96231b3b80d8

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -1214,7 +1214,14 @@ int main(int argc, char **argv, char **envp) {
       return r;
   }
 
+#if defined(KLEE_LIB_DIR) && defined(USE_KLEE_LIB_DIR)
+  /* KLEE_LIB_DIR is the lib dir of installed files as opposed to 
+   * where libs in the klee source tree are generated.
+   */
+  llvm::sys::Path LibraryDir(KLEE_LIB_DIR);
+#else
   llvm::sys::Path LibraryDir(KLEE_DIR "/" RUNTIME_CONFIGURATION "/lib");
+#endif
   Interpreter::ModuleOptions Opts(LibraryDir.c_str(),
                                   /*Optimize=*/OptimizeModule, 
                                   /*CheckDivZero=*/CheckDivZero);
