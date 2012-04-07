dcf55acd75d7a6f74b7b9fa98f84083ea8b79760
Cristian Cadar <cristic@cs.stanford.edu>
2012-04-07 11:37:47 +0000

    Patch by Seungbeom that fixes a memory management issue with Refs, and associated unit test.  More details at http://keeda.stanford.edu/pipermail/klee-commits/2012-February/000904.html
    git-svn-id: https://llvm.org/svn/llvm-project/klee/trunk@154256 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/klee/util/Ref.h
+++ include/klee/util/Ref.h
@@ -32,15 +32,15 @@ public:
   ~ref () { dec (); }
 
 private:
-  void inc() {
+  void inc() const {
     if (ptr)
       ++ptr->refCount;
   }
-  
-  void dec() {
+
+  void dec() const {
     if (ptr && --ptr->refCount == 0)
       delete ptr;
-  }  
+  }
 
 public:
   template<class U> friend class ref;
@@ -49,19 +49,18 @@ public:
   ref(T *p) : ptr(p) {
     inc();
   }
-  
+
   // normal copy constructor
   ref(const ref<T> &r) : ptr(r.ptr) {
     inc();
   }
-  
+
   // conversion constructor
   template<class U>
-  ref (const ref<U> &r) {
-    ptr = r.ptr;
+  ref (const ref<U> &r) : ptr(r.ptr) {
     inc();
   }
-  
+
   // pointer operations
   T *get () const {
     return ptr;
@@ -70,18 +69,18 @@ public:
   /* The copy assignment operator must also explicitly be defined,
    * despite a redundant template. */
   ref<T> &operator= (const ref<T> &r) {
+    r.inc();
     dec();
     ptr = r.ptr;
-    inc();
-    
+
     return *this;
   }
-  
+
   template<class U> ref<T> &operator= (const ref<U> &r) {
+    r.inc();
     dec();
     ptr = r.ptr;
-    inc();
-    
+
     return *this;
   }
 
@@ -130,7 +129,7 @@ struct simplify_type<const ::klee::ref<T> > {
   }
 };
 
-template<typename T> 
+template<typename T>
 struct simplify_type< ::klee::ref<T> >
   : public simplify_type<const ::klee::ref<T> > {};
 }
---
+++ unittests/Ref/RefTest.cpp
@@ -0,0 +1,31 @@
+/* Regression test for a bug caused by assigning a ref to itself.
+   More details at http://keeda.stanford.edu/pipermail/klee-commits/2012-February/000904.html */
+
+#include "gtest/gtest.h"
+#include <iostream>
+#include "klee/util/Ref.h"
+using klee::ref;
+
+int finished = 0;
+
+struct Expr
+{
+  int refCount;
+  Expr() : refCount(0) { 
+    //std::cout << "Expr(" << this << ") created\n"; 
+  }
+  ~Expr() { 
+    //std::cout << "Expr(" << this << ") destroyed\n"; 
+    EXPECT_EQ(finished, 1);
+  }
+};
+
+TEST(RefTest, SelfAssign) 
+{
+  struct Expr *r_e = new Expr();
+  ref<Expr> r(r_e);
+  EXPECT_EQ(r_e->refCount, 1);
+  r = r;
+  EXPECT_EQ(r_e->refCount, 1);
+  finished = 1;
+}
