16e032bb0e5c78c1d718ededc90609b0913ef01b
Martin Nowack <martin.nowack@gmail.com>
2013-08-13 16:30:51 +0200

    Close file descriptors used for warnings and messages

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -336,6 +336,8 @@ KleeHandler::KleeHandler(int argc, char **argv)
 KleeHandler::~KleeHandler() {
   if (m_pathWriter) delete m_pathWriter;
   if (m_symPathWriter) delete m_symPathWriter;
+  fclose(klee_warning_file);
+  fclose(klee_message_file);
   delete m_infoFile;
 }
 
