54b5b1d8fc99376fccbbcb471825012dfa1e4017
Dan Liew <daniel.liew@imperial.ac.uk>
2014-01-14 18:06:52 +0000

    Fixed test cases that fail if using an in-source build

--- test/Coverage/ReadArgs.c
+++ test/Coverage/ReadArgs.c
@@ -1,8 +1,8 @@
 // RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t1.bc
-// RUN: rm -rf xxx
-// RUN: echo " --output-dir=xxx " > %t1.args
+// RUN: rm -rf %T/xxx
+// RUN: echo " --output-dir=%T/xxx " > %t1.args
 // RUN: %klee --read-args %t1.args %t1.bc
-// RUN: test -d xxx
+// RUN: test -d %T/xxx
 
 int main() {
   return 0;
