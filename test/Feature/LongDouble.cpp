ad8b548626ba5b797f948200cff2d80d579a12c2
Dan Liew <daniel.liew@imperial.ac.uk>
2014-01-20 14:45:50 +0000

    Fixed Feature/LongDouble.cpp test for llvm-lit by removed use of { } quotes. I also add FileCheck lines but I've not added running FileCheck because only new versions of FileCheck support the CHECK-DAG: syntax.

--- test/Feature/LongDouble.cpp
+++ test/Feature/LongDouble.cpp
@@ -1,11 +1,11 @@
 // RUN: %llvmgxx -I../../../include -g -fno-exceptions -emit-llvm -O0 -c -o %t.bc %s
 // RUN: %klee --libc=klee --no-output --exit-on-error %t.bc > %t.log
-// RUN: grep -q {powl\(-11\\.0,0\)=1\\.0\\+} %t.log
-// RUN: grep -q {powl\(-11\\.0,1\)=-11\\.0\\+} %t.log
-// RUN: grep -q {powl\(-11\\.0,2\)=121\\.0\\+} %t.log
-// RUN: grep -q {1/0=inf} %t.log
-// RUN: grep -q {1/-1=-1\\.0\\+} %t.log
-// RUN: grep -q {1/-2=-0\\.50\\+} %t.log
+// RUN: grep -q powl\(-11\\.0,0\)=1\\.0\\+ %t.log
+// RUN: grep -q powl\(-11\\.0,1\)=-11\\.0\\+ %t.log
+// RUN: grep -q powl\(-11\\.0,2\)=121\\.0\\+ %t.log
+// RUN: grep -q 1/0=inf %t.log
+// RUN: grep -q 1/-1=-1\\.0\\+ %t.log
+// RUN: grep -q 1/-2=-0\\.50\\+ %t.log
 
 #include <cstdio>
 #include <cstdlib>
@@ -44,10 +44,17 @@ int main(int argc, char ** argv)
 
   // test 80-bit external dispatch
   long double d = powl((long double)-11.0, (long double)a);
+  // FIXME: Use CHECK-DAG: with FileCheck tool
+  // CHECK-DAG: powl(-11.0,0)=1.0
+  // CHECK-DAG: powl(-11.0,1)=-11.0
+  // CHECK-DAG: powl(-11.0,2)=121.0
   printf("powl(-11.0,%d)=%Lf\n", a, d);
 
   // test 80-bit fdiv
   long double e = (long double) 1 / (long double) b;
+  // CHECK-DAG: 1/0=inf
+  // CHECK-DAG: 1/1-1=-1.0
+  // CHECK-DAG: 1/-2=-0.50
   printf("1/%d=%Lf\n", b, e);
 
   return 0;
