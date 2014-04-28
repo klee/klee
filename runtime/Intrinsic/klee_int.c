cc8b79f73bcba26830d26848e65efc6cce752043
Martin Nowack <martin@se.inf.tu-dresden.de>
2014-04-28 11:42:08 +0200

    [runtime] Fix include in klee_int

--- runtime/Intrinsic/klee_int.c
+++ runtime/Intrinsic/klee_int.c
@@ -8,7 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 #include <assert.h>
-#include <klee/klee.h>
+#include "klee/klee.h"
 
 int klee_int(const char *name) {
   int x;
