892543cc6d6590b612a83e8cac58637f8c223b46
Cristian Cadar <c.cadar@imperial.ac.uk>
2014-09-13 12:37:23 +0100

    These tests require --optimize=false to run successfully.

--- test/Feature/DanglingConcreteReadExpr.c
+++ test/Feature/DanglingConcreteReadExpr.c
@@ -1,6 +1,6 @@
 // RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out %t1.bc
+// RUN: %klee --optimize=false --output-dir=%t.klee-out %t1.bc
 // RUN: grep "total queries = 2" %t.klee-out/info
 
 #include <assert.h>
--- test/Feature/MakeConcreteSymbolic.c
+++ test/Feature/MakeConcreteSymbolic.c
@@ -1,10 +1,10 @@
 // RUN: %llvmgcc %s -emit-llvm -g -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --exit-on-error %t1.bc
+// RUN: %klee --optimize=false --output-dir=%t.klee-out --exit-on-error %t1.bc
 // RUN: grep "done: total queries = 0" %t.klee-out/info
 
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --make-concrete-symbolic=1 --exit-on-error %t1.bc
+// RUN: %klee --optimize=false --output-dir=%t.klee-out --make-concrete-symbolic=1 --exit-on-error %t1.bc
 // RUN: grep "done: total queries = 2" %t.klee-out/info
 
 
