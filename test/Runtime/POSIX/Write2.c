a04f2277f90dc2aac3a3277c9dd7c6d3378059e4
Martin Nowack <martin@se.inf.tu-dresden.de>
2014-09-14 21:47:34 +0200

    Enable test case again

--- test/Runtime/POSIX/Write2.c
+++ test/Runtime/POSIX/Write2.c
@@ -1,12 +1,6 @@
 // RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --exit-on-error --libc=uclibc --posix-runtime %t.bc --sym-files 1 10 --sym-stdout 2>%t.log
-//
-// Disable this test on LLVM 3.4 for now, it seems to hang indefinitely when run
-// in our Travis CI config.
-//
-// FIXME: Investigate.
-// REQUIRES: not-llvm-3.4
 
 #include <stdio.h>
 #include <assert.h>
