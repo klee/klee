4af13aa1ae965a3fff9f005a78674efdc49ce3f7
Willem <willem@lekkertech.net>
2014-10-09 23:17:16 -0700

    Fixed passing of long double (and other big types) in var_args on x86_64. Removed XFAIL tag from the Feature/VarArgLongDouble.c test
    Fixed Executor to (more) correctly handle the alignment of types larger than 64bit (such as long double) when those are passed in var_args on x86_64.
    
    Specifically:
    From http://www.x86-64.org/documentation/abi.pdf
    AMD64-ABI 3.5.7p5: Step 7.
    Align l->overflow_arg_area upwards to a 16 byte boundary if alignment needed by type exceeds 8 byte boundary.

--- test/Feature/VarArgLongDouble.c
+++ test/Feature/VarArgLongDouble.c
@@ -1,7 +1,6 @@
 // RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out %t.bc | FileCheck %s
-// XFAIL:
 
 #include <stdarg.h>
 #include <assert.h>
