28821122b625c5eefbda09a0f28c575d47136f1f
Dan Liew <daniel.liew@imperial.ac.uk>
2015-04-15 21:45:30 +0100

    Remove boost requirements from TravisCI build.

--- .travis/stp.sh
+++ .travis/stp.sh
@@ -21,12 +21,12 @@ if [ "${STP_VERSION}" == "UPSTREAM" ]; then
     git clone --depth 1 git://github.com/stp/stp.git src
     mkdir build
     cd build
-    # Disabling building of shared libs is a workaround
-    cmake -DBUILD_SHARED_LIBS:BOOL=OFF -DENABLE_PYTHON_INTERFACE:BOOL=OFF ../src
-    # Don't try to build stp executable, there's an issue with using gcc4.8 with boost libraries built with gcc4.6
+    # Disabling building of shared libs is a workaround.
+    # Don't build against boost because that is broken when mixing packaged boost libraries and gcc 4.8
+    cmake -DBUILD_SHARED_LIBS:BOOL=OFF -DENABLE_PYTHON_INTERFACE:BOOL=OFF -DNO_BOOST:BOOL=ON ../src
 
     set +e # Do not exit if build fails because we need to display the log
-    make libstp CopyPublicHeaders >> "${STP_LOG}" 2>&1
+    make >> "${STP_LOG}" 2>&1
 
 elif [ "${STP_VERSION}" == "r940" ]; then
     # Building the old "r940" version that for some reason we love so much!
