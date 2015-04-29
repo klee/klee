672866d94be4962330cca3f35261fc164a40441e
Chace Clark <chace3@neo.tamu.edu>
2015-04-29 14:33:06 -0500

    removed unused variable from StatsTracker.cpp trying to make TravisCI happy

--- lib/Core/StatsTracker.cpp
+++ lib/Core/StatsTracker.cpp
@@ -189,7 +189,6 @@ StatsTracker::StatsTracker(Executor &_executor, std::string _objectFilename,
   if (!sys::path::is_absolute(objectFilename)) {
     SmallString<128> current(objectFilename);
     if(sys::fs::make_absolute(current)) {
-      bool exists = false;
 #if LLVM_VERSION_CODE >= LLVM_VERSION(3, 6)
       Twine current_twine(current.str()); //requires a twine for this. so silly	
       if (!sys::fs::exists(current_twine)) {
