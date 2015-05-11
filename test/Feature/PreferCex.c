33417e4b63377e2825811096aaca026847cf2e26
Eric Rizzi <eric.rizzi@gmail.com>
2015-05-11 10:34:05 -0400

    Make use of prefer-cex optional rather than default
    Previously, default Klee would go through every byte in a test case
    and attempt to bound it to be between 0 and 127, making it human
    readable.  While this may be useful when attempting to understand Klee,
    it also means that the time required to create large test suites was
    greatly increased.  By making this behavior default off, unsuspecting
    users won't incur these additional costs.

--- test/Feature/PreferCex.c
+++ test/Feature/PreferCex.c
@@ -1,6 +1,6 @@
 // RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --exit-on-error %t1.bc
+// RUN: %klee --output-dir=%t.klee-out --exit-on-error --prefer-cex %t1.bc
 // RUN: ktest-tool %t.klee-out/test000001.ktest | FileCheck %s
 
 #include <assert.h>
