46e4061d78ce7567253fc7774b46647221f04f03
Martin Nowack <martin.nowack@gmail.com>
2015-08-09 23:44:43 +0200

    Add dump function for Assignment

--- include/klee/util/Assignment.h
+++ include/klee/util/Assignment.h
@@ -49,6 +49,15 @@ namespace klee {
 
     template<typename InputIterator>
     bool satisfies(InputIterator begin, InputIterator end);
+
+    void dump() {
+      for (bindings_ty::iterator i = bindings.begin(), e = bindings.end(); i != e; ++i) {
+        llvm::errs() << (*i).first->name << "\n[";
+        for (int j = 0, k =(*i).second.size(); j<k; ++j )
+          llvm::errs() << (int)(*i).second[j] << ",";
+        llvm::errs() << "]\n";
+      }
+    }
   };
   
   class AssignmentEvaluator : public ExprEvaluator {
