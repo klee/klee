238998f8e17b554d0c28931a61271619310a098f
Dan Liew <daniel.liew@imperial.ac.uk>
2016-02-23 17:21:42 +0000

    Move ``Assignment::dump()`` into its own implementation file so that it's possible to call it from gdb.

---
+++ lib/Expr/Assigment.cpp
@@ -0,0 +1,21 @@
+//===-- Assignment.cpp ----------------------------------------------------===//
+//
+//                     The KLEE Symbolic Virtual Machine
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include "klee/util/Assignment.h"
+namespace klee {
+
+void Assignment::dump() {
+  for (bindings_ty::iterator i = bindings.begin(), e = bindings.end(); i != e;
+       ++i) {
+    llvm::errs() << (*i).first->name << "\n[";
+    for (int j = 0, k = (*i).second.size(); j < k; ++j)
+      llvm::errs() << (int)(*i).second[j] << ",";
+    llvm::errs() << "]\n";
+  }
+}
+}
