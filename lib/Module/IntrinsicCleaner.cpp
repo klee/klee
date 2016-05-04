fad40b6c2651f4a90700df879467513adf30992b
Richard Trembecký <richardt@centrum.sk>
2016-05-04 15:21:45 +0200

    llvm38: no more implicit iterators
    LLVM commit eac309550f25 removed implicit iterator conversions. So we
    have to get the iterators explicitly now.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- lib/Module/IntrinsicCleaner.cpp
+++ lib/Module/IntrinsicCleaner.cpp
@@ -110,7 +110,11 @@ bool IntrinsicCleanerPass::runOnBasicBlock(BasicBlock &b, Module &M) {
       case Intrinsic::uadd_with_overflow:
       case Intrinsic::usub_with_overflow:
       case Intrinsic::umul_with_overflow: {
+#if LLVM_VERSION_CODE >= LLVM_VERSION(3, 8)
+        IRBuilder<> builder(ii->getParent(), ii->getIterator());
+#else
         IRBuilder<> builder(ii->getParent(), ii);
+#endif
 
         Value *op1 = ii->getArgOperand(0);
         Value *op2 = ii->getArgOperand(1);
