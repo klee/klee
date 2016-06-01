c404c47e209640c3613aa58c2545212518147a3c
Andrew Santosa <dcsandr@nus.edu.sg>
2016-06-01 15:47:46 +0800

    Removed checking for pointer type in Dependency::getLatestValue
    The issue that was fixed by this is due to the lack of handling of getenv()
    system call. As the system call is now handled in Dependency::execute, this
    fix is no longer necessary.

--- lib/Core/Dependency.cpp
+++ lib/Core/Dependency.cpp
@@ -545,10 +545,6 @@ VersionedValue *Dependency::getLatestValue(llvm::Value *value,
   if (parentDependency)
     return parentDependency->getLatestValue(value, valueExpr);
 
-  if (llvm::isa<llvm::PointerType>(value->getType())) {
-    return getNewVersionedValue(value, valueExpr);
-  }
-
   return 0;
 }
 
