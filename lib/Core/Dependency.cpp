37d9b737c4345ab087ea7eb2cb04ea9bd6320ec2
Andrew Santosa <dcsandr@nus.edu.sg>
2016-12-12 16:32:18 +0800

    Added -no-bound-interpolation option

--- lib/Core/Dependency.cpp
+++ lib/Core/Dependency.cpp
@@ -865,7 +865,8 @@ void Dependency::executeMemoryOperation(llvm::Instruction *instr,
                                         bool boundsCheck,
                                         bool symbolicExecutionError) {
   execute(instr, args, symbolicExecutionError);
-  if (boundsCheck) {
+#ifdef ENABLE_Z3
+  if (!NoBoundInterpolation && boundsCheck) {
     // The bounds check has been proven valid, we keep the dependency on the
     // address. Calling va_start within a variadic function also triggers memory
     // operation, but we ignored it here as this method is only called when load
@@ -887,6 +888,7 @@ void Dependency::executeMemoryOperation(llvm::Instruction *instr,
     }
     markAllValues(addressOperand);
   }
+#endif
 }
 
 void Dependency::bindCallArguments(llvm::Instruction *i,
