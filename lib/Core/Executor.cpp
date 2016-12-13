a262d80558d52f2b208ccb70e9d6450f100d1b5e
Dan Liew <daniel.liew@imperial.ac.uk>
2016-12-13 23:22:03 +0000

    Fix bug where the incorrect amount of memory was allocated for padded types (e.g. `long double` on x86). Previously we would report an out of bound access if an attempt was made to access the padding.
    This also fixes indexing into padded types which was incorrect
    previously because padding was not included.
    
    Two test cases are included. Unfortunately I couldn't
    find a way to test `ConstantDataSequential` or `ConstantAggregateZero`
    from a test case.
    
    It also appears handling of vectors is broken in various ways
    so those parts of the test cases are disabled until we can fix
    them.

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -458,17 +458,17 @@ void Executor::initializeGlobalObject(ExecutionState &state, ObjectState *os,
 #endif
   if (const ConstantVector *cp = dyn_cast<ConstantVector>(c)) {
     unsigned elementSize =
-      targetData->getTypeStoreSize(cp->getType()->getElementType());
+        targetData->getTypeAllocSize(cp->getType()->getElementType());
     for (unsigned i=0, e=cp->getNumOperands(); i != e; ++i)
-      initializeGlobalObject(state, os, cp->getOperand(i), 
-			     offset + i*elementSize);
+      initializeGlobalObject(state, os, cp->getOperand(i),
+                             offset + (i * elementSize));
   } else if (isa<ConstantAggregateZero>(c)) {
-    unsigned i, size = targetData->getTypeStoreSize(c->getType());
+    unsigned i, size = targetData->getTypeAllocSize(c->getType());
     for (i=0; i<size; i++)
       os->write8(offset+i, (uint8_t) 0);
   } else if (const ConstantArray *ca = dyn_cast<ConstantArray>(c)) {
     unsigned elementSize =
-      targetData->getTypeStoreSize(ca->getType()->getElementType());
+        targetData->getTypeAllocSize(ca->getType()->getElementType());
     for (unsigned i=0, e=ca->getNumOperands(); i != e; ++i)
       initializeGlobalObject(state, os, ca->getOperand(i), 
 			     offset + i*elementSize);
@@ -481,20 +481,18 @@ void Executor::initializeGlobalObject(ExecutionState &state, ObjectState *os,
 #if LLVM_VERSION_CODE >= LLVM_VERSION(3, 1)
   } else if (const ConstantDataSequential *cds =
                dyn_cast<ConstantDataSequential>(c)) {
-    unsigned elementSize =
-      targetData->getTypeStoreSize(cds->getElementType());
+    unsigned elementSize = targetData->getTypeAllocSize(cds->getElementType());
     for (unsigned i=0, e=cds->getNumElements(); i != e; ++i)
       initializeGlobalObject(state, os, cds->getElementAsConstant(i),
                              offset + i*elementSize);
 #endif
   } else if (!isa<UndefValue>(c)) {
-    unsigned StoreBits = targetData->getTypeStoreSizeInBits(c->getType());
+    unsigned allocBits = targetData->getTypeAllocSizeInBits(c->getType());
     ref<ConstantExpr> C = evalConstant(c);
-
-    // Extend the constant if necessary;
-    assert(StoreBits >= C->getWidth() && "Invalid store size!");
-    if (StoreBits > C->getWidth())
-      C = C->ZExt(StoreBits);
+    // Extend the constant, zero padding if necessary
+    assert(allocBits >= C->getWidth() && "alloc size invalid");
+    if (allocBits > C->getWidth())
+      C = C->ZExt(allocBits);
 
     os->write(offset, C);
   }
@@ -593,10 +591,10 @@ void Executor::initializeGlobals(ExecutionState &state) {
       LLVM_TYPE_Q Type *ty = i->getType()->getElementType();
       uint64_t size = 0;
       if (ty->isSized()) {
-	size = kmodule->targetData->getTypeStoreSize(ty);
+        size = kmodule->targetData->getTypeAllocSize(ty);
       } else {
         klee_warning("Type for %.*s is not sized", (int)i->getName().size(),
-			i->getName().data());
+                     i->getName().data());
       }
 
       // XXX - DWD - hardcode some things until we decide how to fix.
@@ -638,7 +636,7 @@ void Executor::initializeGlobals(ExecutionState &state) {
       }
     } else {
       LLVM_TYPE_Q Type *ty = i->getType()->getElementType();
-      uint64_t size = kmodule->targetData->getTypeStoreSize(ty);
+      uint64_t size = kmodule->targetData->getTypeAllocSize(ty);
       MemoryObject *mo = memory->allocate(size, false, true, &*i);
       if (!mo)
         llvm::report_fatal_error("out of memory");
@@ -2095,8 +2093,8 @@ void Executor::executeInstruction(ExecutionState &state, KInstruction *ki) {
     // Memory instructions...
   case Instruction::Alloca: {
     AllocaInst *ai = cast<AllocaInst>(i);
-    unsigned elementSize = 
-      kmodule->targetData->getTypeStoreSize(ai->getAllocatedType());
+    unsigned elementSize =
+        kmodule->targetData->getTypeAllocSize(ai->getAllocatedType());
     ref<Expr> size = Expr::createPointer(elementSize);
     if (ai->isArrayAllocation()) {
       ref<Expr> count = eval(ki, 0, state).value;
@@ -2595,8 +2593,8 @@ void Executor::computeOffsets(KGEPInstruction *kgepi, TypeIt ib, TypeIt ie) {
                                                                Context::get().getPointerWidth()));
     } else {
       const SequentialType *set = cast<SequentialType>(*ii);
-      uint64_t elementSize = 
-        kmodule->targetData->getTypeStoreSize(set->getElementType());
+      uint64_t elementSize =
+          kmodule->targetData->getTypeAllocSize(set->getElementType());
       Value *operand = ii.getOperand();
       if (Constant *c = dyn_cast<Constant>(operand)) {
         ref<ConstantExpr> index = 
