4045d97e2922fa7c97b2762c7df9bf71a76feb32
Andrew Santosa <dcsandr@nus.edu.sg>
2017-03-02 13:47:25 +0800

    Register tracerx_push_debug_level and tracerx_pop_debug_level as modeled

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -911,6 +911,8 @@ static const char *modelledExternals[] = {
   "__ubsan_handle_sub_overflow",
   "__ubsan_handle_mul_overflow",
   "__ubsan_handle_divrem_overflow",
+  "tracerx_push_debug_level",
+  "tracerx_pop_debug_level"
 };
 // Symbols we aren't going to warn about
 static const char *dontCareExternals[] = {
