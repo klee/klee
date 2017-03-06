959a810cffa91f896021ae2148eced373f283670
Andrea Mattavelli <andreamattavelli@gmail.com>
2017-03-06 15:17:14 +0000

    [Travis-CI] Refactored Z3 in its own script

--- .travis/solvers.sh
+++ .travis/solvers.sh
@@ -16,15 +16,8 @@ for solver in ${SOLVER_LIST}; do
     cd ../
     ;;
   Z3)
-    # FIXME: Move this into its own script
-    source ${KLEE_SRC}/.travis/sanitizer_flags.sh
-    if [ "X${IS_SANITIZED_BUILD}" != "X0" ]; then
-      echo "Error: Requested Sanitized build but Z3 being used is not sanitized"
-      exit 1
-    fi
     echo "Z3"
-    # Should we install libz3-dbg too?
-    sudo apt-get -y install libz3 libz3-dev
+    ${KLEE_SRC}/.travis/z3.sh
     ;;
   metaSMT)
     echo "metaSMT"
