a0a94cfa1a6b8309bd65ca50761fe21bc214f7a7
Martin Nowack <martin@se.inf.tu-dresden.de>
2017-07-22 22:28:52 +0200

    Cleanup Travis builder

--- .travis/klee.sh
+++ .travis/klee.sh
@@ -9,29 +9,15 @@ SVN_BRANCH="release_$( echo ${LLVM_VERSION} | sed 's/\.//g')"
 ###############################################################################
 # Select the compiler to use to generate LLVM bitcode
 ###############################################################################
-if [ "${LLVM_VERSION}" != "2.9" ]; then
-  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    KLEE_CC=/usr/bin/clang-${LLVM_VERSION}
-    KLEE_CXX=/usr/bin/clang++-${LLVM_VERSION}
-  elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-    KLEE_CC=/usr/local/bin/clang-${LLVM_VERSION}
-    KLEE_CXX=/usr/local/bin/clang++-${LLVM_VERSION}
-  else
-    echo "Unhandled TRAVIS_OS_NAME \"${TRAVIS_OS_NAME}\""
-    exit 1
-  fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  KLEE_CC=/usr/bin/clang-${LLVM_VERSION}
+  KLEE_CXX=/usr/bin/clang++-${LLVM_VERSION}
+elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+  KLEE_CC=/usr/local/bin/clang-${LLVM_VERSION}
+  KLEE_CXX=/usr/local/bin/clang++-${LLVM_VERSION}
 else
-    # Just use pre-built llvm-gcc downloaded earlier
-    KLEE_CC=${BUILD_DIR}/llvm-gcc/bin/llvm-gcc
-    KLEE_CXX=${BUILD_DIR}/llvm-gcc/bin/llvm-g++
-    export C_INCLUDE_PATH=/usr/include/x86_64-linux-gnu
-    export CPLUS_INCLUDE_PATH=/usr/include/x86_64-linux-gnu
-
-    # Add symlinks to fix llvm-2.9-dev package so KLEE can configure properly
-    # Because of the way KLEE's configure script works this must be a relative
-    # symlink, **not** absolute!
-    sudo sh -c 'cd /usr/lib/llvm-2.9/build/ && ln -s ../ Release'
-    sudo sh -c 'cd /usr/lib/llvm-2.9/build/ && ln -s ../include include'
+  echo "Unhandled TRAVIS_OS_NAME \"${TRAVIS_OS_NAME}\""
+  exit 1
 fi
 
 ###############################################################################
