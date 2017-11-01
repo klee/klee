f8c92ef6c605c20affc34758a8386ed20ecfa36f
Frank Busse <bb0xfb@gmail.com>
2017-11-01 20:56:01 +0000

    klee_make_symbolic: add test cases for API

---
+++ test/regression/2017-11-01-test-with-empty-varname.c
@@ -0,0 +1,11 @@
+// RUN: %llvmgcc %s -emit-llvm -g -c -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out %t1.bc
+// RUN: FileCheck %s --input-file=%t.klee-out/warnings.txt
+
+int main() {
+  unsigned a;
+
+  klee_make_symbolic(&a, sizeof(a), "");
+// CHECK-NOT: KLEE: WARNING: unable to write output test case, losing it
+}
