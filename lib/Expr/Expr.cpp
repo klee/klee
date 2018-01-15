5db9454e68bc0f4ce7afbb0077fa65d3d9a95b1f
Jiri Slaby <jirislaby@gmail.com>
2018-01-15 10:09:20 +0100

    llvm5: integerPartWidth is from llvm::APFloatBase
    Otherwise we see:
    ../lib/Expr/Expr.cpp:331:14: error: no member named 'integerPartWidth' in namespace 'llvm'; did you mean 'llvm::APFloatBase::integerPartWidth'?
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- lib/Expr/Expr.cpp
+++ lib/Expr/Expr.cpp
@@ -333,7 +333,11 @@ ref<Expr> ConstantExpr::fromMemory(void *address, Width width) {
   // FIXME: what about machines without x87 support?
   default:
     return ConstantExpr::alloc(llvm::APInt(width,
+#if LLVM_VERSION_CODE >= LLVM_VERSION(5, 0)
+      (width+llvm::APFloatBase::integerPartWidth-1)/llvm::APFloatBase::integerPartWidth,
+#else
       (width+llvm::integerPartWidth-1)/llvm::integerPartWidth,
+#endif
       (const uint64_t*)address));
   }
 }
