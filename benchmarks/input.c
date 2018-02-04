b8f5b2a64d0d699e9894d9c307ace1320b5f3d92
OrenGitHub <tuna.is.good.for.you@gmail.com>
2018-02-04 11:38:34 +0200

    finalized sed script and some re-writes to input.c

--- benchmarks/input.c
+++ benchmarks/input.c
@@ -21,59 +21,7 @@ void MyConstStringAssign(char *p,const char *q);
 void MyStringAssignWithOffset(char *p, char *q,int offset1);
 void MyWriteCharToStringAtOffset(char *p,int offset2,char c);
 
-char *osip_clrncpy (char *dst, const char *src, size_t len)
-{
-	const char *pbeg;
-	const char *pend;
-	char *p;
-	size_t spaceless_length;
-
-	if (src == NULL)
-		return NULL;
-
-	/* find the start of relevant text */
-	pbeg = src;
-	while ((' ' == *pbeg) || ('\r' == *pbeg) || ('\n' == *pbeg) || ('\t' == *pbeg))
-		pbeg++;
-
-
-	/* find the end of relevant text */
-	pend = src + len - 1;
-	while ((' ' == *pend) || ('\r' == *pend) || ('\n' == *pend) || ('\t' == *pend))
-	{
-		pend--;
-		if (pend < pbeg)
-		{
-			*dst = '\0';
-			return dst;
-		}
-	}
-
-	/* if pend == pbeg there is only one char to copy */
-	spaceless_length = pend - pbeg + 1;   /* excluding any '\0' */
-	memmove (dst, pbeg, spaceless_length);
-	p = dst + spaceless_length;
-
-	/* terminate the string and pad dest with zeros until len */
-	do
-	{
-		*p = '\0';
-		p++;
-		spaceless_length++;
-	}
-	while (spaceless_length < len);
-
-	return dst;
-}
-
-char *osip_strncpy (char *dest, const char *src, size_t length)
-{
-	strncpy (dest, src, length);
-	dest[length] = '\0';
-	return dest;
-}
-
-void MyPrintOutput(char *s)
+void MyMyPrintOutput(char *s)
 {
 	fprintf(stdout,"%s\n",s);
 }
@@ -83,38 +31,50 @@ void MyPrintOutput(char *s)
 int main(int argc, char **argv)
 {
 	int   i1;
-	char *p1;
-	char *p3;
-	char *p4;
-	char *p5;
-	char *p6;
-	char *p7;
-	char *p8;
-	char *p9;
-	char *p10;
-	char *p11;
-	char *p12;
-	char *p13;
-	char *p14;
-	char *p15;
-	char *p16;
-	char *p17;
-	char *p18;
-	char *hp;
-	char *q1 = "SIP/";
+	int   i2;
+	int   i3;
+	int   i4;
+	int   i5;
+	int   i6;
+	int   i7;
+	int   i8;
+	int   i9;
+	int  i10;
+	int  i11;
+	int  i12;
+	int  i13;
+	char * p1;
+	char * p3;
+	char * p4;
+	char * p5;
+	char * p6;
+	char * p7;
+	char * p8;
+	char * p9;
+	char * p10;
+	char * p11;
+	char * p12;
+	char * p13;
+	char * p14;
+	char * p15;
+	char * p16;
+	char * p17;
+	char * p18;
+	char * p20;
+	char * p21;
+	char * p22;
+	char * p23;
+	char * q1 = "SIP/";
 
-	char msg[700];
+	// char msg[700];
 
 	///////////////////////////////
-	// p1 = (char *) malloc(700);//
+	//klee_make_symbolic(msg,700,"msg");
+	//p1 = msg;
 	///////////////////////////////
-	klee_make_symbolic(msg,700,"msg");
-	p1 = msg;
+	p1 = malloc(32);
 
-	p1[696] = '\0';
-	p1[697] = '\0';
-	p1[698] = '\0';
-	p1[699] = '\0';
+	p1[31] = 0;
 
 	/* skip initial \r\n */
 	while (p1[0] == '\r' || p1[0] == '\n')
@@ -122,12 +82,12 @@ int main(int argc, char **argv)
 		p1++;
 	}
 
-	for (; p1[0] != '\0'; p1 = p1+1)
+	for (; p1[0] != 0; p1 = p1+1)
 	{
-		if (('\0' == p1[0]) ||
-			('\0' == p1[1]) ||
-			('\0' == p1[2]) ||
-			('\0' == p1[3]))
+		if ((0 == p1[0]) ||
+			(0 == p1[1]) ||
+			(0 == p1[2]) ||
+			(0 == p1[3]))
 		{
 			return 0;
 		}
@@ -160,56 +120,47 @@ int main(int argc, char **argv)
 	if (strncmp(p1,q1,4) != 0)
 	{
 		p6 = p1;
-
 		/* search for first SPACE */
 		p3 = strchr (p1, ' ');
 		if (p3 == NULL)
 		{
-			MyPrintOutput("OSIP_SYNTAXERROR 1");
-			//fprintf(stdout,"OSIP_SYNTAXERROR 1");
+			MyMyPrintOutput("OSIP_SYNTAXERROR 1");
+			return 0;
 		}
-		p5 = (char *) malloc (p3 - p6 + 1);
-		strncpy (p5, p6, p3 - p6);
-
-		p4 = strchr (p3 + 1, ' ');
+		i1 = p3 - p1;
+		i2 = p3 - p6;
+		i3 = i1 +  1;
+		p5 = malloc(i3);
+		strncpy (p5, p6, i2);
+		p14 = p3 + 1;
+		p4 = strchr (p14, ' ');
 		if (p4 == NULL)
 		{
-			MyPrintOutput("OSIP_SYNTAXERROR 2");
-			//fprintf(stdout,"OSIP_SYNTAXERROR 2");
+			MyMyPrintOutput("OSIP_SYNTAXERROR 2");
+			return 0;
 		}
-		if (sscanf (p3 + 1, "%d", &i1) != 1)
-		{
-			/* Non-numeric status code */
-			MyPrintOutput("OSIP_SYNTAXERROR 3");
-			//fprintf(stdout,"OSIP_SYNTAXERROR 3");
-		}
-
-		if (i1 == 0)
-		{
-			MyPrintOutput("OSIP_SYNTAXERROR 4");
-			//fprintf(stdout,"OSIP_SYNTAXERROR 4");
-		}
-
 		{
 			p7 = p4;
 
-			while ((*p7 != '\r') && (*p7 != '\n'))
+			while ((p7[0] != '\r') && (p7[0] != '\n'))
 			{
-				if (*p7)
+				if (p7[0] != 0)
 				{
 					p7++;
 				}
 				else
 				{
-					MyPrintOutput("No crlf found\n");
-					// fprintf(stdout,"No crlf found\n");
+					MyMyPrintOutput("No crlf found\n");
 				}
     		}
-			p8 = (char *) malloc (p7 - p4);
-			strncpy (p8, p4 + 1, p7 - p4 - 1);
+			i4 = p7 - p4;
+			i5 = i4 - 1;
+			p8 = malloc(i4);
+			p15 = p4 + 1;
+			strncpy (p8, p15, i5);
 
 			p7++;
-			if ((*p7) && ('\r' == p7[-1]) && ('\n' == p7[0]))
+			if ((p7[0] != 0) && ('\r' == p7[-1]) && ('\n' == p7[0]))
 			{
 				p7++;
 			}
@@ -221,108 +172,104 @@ int main(int argc, char **argv)
 		p12 = strchr (p1, ' ');
 		if (p12 == NULL)
 		{
-			MyPrintOutput("OSIP_SYNTAXERROR 5\n");
-			//fprintf(stdout,"OSIP_SYNTAXERROR 5\n");
+			MyMyPrintOutput("OSIP_SYNTAXERROR 5\n");
 		}
-		if (*(p12 + 1) == '\0' || *(p12 + 2) == '\0')
+		if (p12[1] == 0 || p12[2] == 0)
 		{
-			MyPrintOutput("OSIP_SYNTAXERROR 6\n");
-			//fprintf(stdout,"OSIP_SYNTAXERROR 6\n");
+			MyMyPrintOutput("OSIP_SYNTAXERROR 6\n");
 		}
-		if (p12 - p1 == 0)
+		i6 = p12 - p1;
+		if (i6 == 0)
 		{
-			MyPrintOutput("No space allowed here\n");
-			//fprintf(stdout,"No space allowed here\n");
+			MyMyPrintOutput("No space allowed here\n");
 		}
-		p13 = (char *) malloc (p12 - p1 + 1);
-		osip_strncpy (p13, p1, p12 - p1);
+		i7 = i6 + 1;
+		p13 = malloc(i7);
+		strncpy (p13, p1, i6);
+		p13[i6] = 0;
 
 		/* The second token is a sip-url or a uri: */
-		p11 = strchr (p12 + 2, ' ');/* no space allowed inside sip-url */
+		p21 = p12 + 2;
+		p11 = strchr (p21, ' ');/* no space allowed inside sip-url */
 		if (p11 == NULL)
 		{
-			MyPrintOutput("Uncompliant request-uri\n");
-			//fprintf(stdout,"Uncompliant request-uri\n");
+			MyMyPrintOutput("Uncompliant request-uri\n");
 		}
-		if (p11 - p12 < 2)
+		i10 = p11 - p12;
+		if (i10 < 2)
 		{
-			MyPrintOutput("OSIP_SYNTAXERROR 7\n");
-			//fprintf(stdout,"OSIP_SYNTAXERROR 7\n");
+			MyMyPrintOutput("OSIP_SYNTAXERROR 7\n");
 		}
-
-		p17 = (char *) malloc (p11 - p12);
-		osip_clrncpy (p17, p12 + 1, (p11 - p12 - 1));
 
 		/* find the the version and the beginning of headers */
-		hp = p11;
+		p20 = p11;
 
-		hp++;                       /* skip space */
-		if (*hp == '\0'       ||
-			*(hp + 1) == '\0' ||
-			*(hp + 2) == '\0' ||
-			*(hp + 3) == '\0' ||
-			*(hp + 4) == '\0' ||
-			*(hp + 5) == '\0' ||
-			*(hp + 6) == '\0')
+		p20++;                       /* skip space */
+		if (p20[0] == 0 ||
+			p20[1] == 0 ||
+			p20[2] == 0 ||
+			p20[3] == 0 ||
+			p20[4] == 0 ||
+			p20[5] == 0 ||
+			p20[6] == 0)
 		{
-			MyPrintOutput("Uncomplete request line\n");
-			//fprintf(stdout,"Uncomplete request line\n");
+			MyMyPrintOutput("Uncomplete request line\n");
 		}
-		if (((hp[0] != 'S') && (hp[0] != 's')) ||
-			((hp[1] != 'I') && (hp[1] != 'i')) ||
-			((hp[2] != 'P') && (hp[2] != 'p')) ||
-			(hp[3] != '/'))
+		if (((p20[0] != 'S') && (p20[0] != 's')) ||
+			((p20[1] != 'I') && (p20[1] != 'i')) ||
+			((p20[2] != 'P') && (p20[2] != 'p')) ||
+			(p20[3] != '/'))
 		{
-			MyPrintOutput("No crlf found/No SIP/2.0 found\n");
-			//fprintf(stdout,"No crlf found/No SIP/2.0 found\n");
+			MyMyPrintOutput("No crlf found/No SIP/2.0 found\n");
 		}
+
 		/* SIP/ */
-		hp = hp + 4;
+		p20 = p20 + 4;
 
-		while ((*hp != '\r') && (*hp != '\n'))
+		while ((p20[0] != '\r') && (p20[0] != '\n'))
 		{
-			if (*hp)
+			if (p20[0] != 0)
 			{
-				if ((*hp >= '0') && (*hp <= '9'))
+				if ((p20[0] >= '0') && (p20[0] <= '9'))
 				{
-					hp++;
+					p20++;
 				}
-				else if (*hp == '.')
+				else if (p20[0] == '.')
 				{
-					hp++;
+					p20++;
 				}
 				else
 				{
-					MyPrintOutput("incorrect sip version string\n");
-					//fprintf(stdout,"incorrect sip version string\n");
+					MyMyPrintOutput("incorrect sip version string\n");
 				}
 			}
 			else
 			{
-				MyPrintOutput("No crlf found\n");
-				//fprintf(stdout,"No crlf found\n");
+				MyMyPrintOutput("No crlf found\n");
 			}
 		}
-		if (hp - p11 < 2)
+		i8 = p20 - p11;
+		if (i8 < 2)
 		{
-			MyPrintOutput("OSIP_SYNTAXERROR 8");
-			//fprintf(stdout,"OSIP_SYNTAXERROR 8");
+			MyMyPrintOutput("OSIP_SYNTAXERROR 8");
 		}
 
-		p18 = (char *) malloc (hp - p11);
-		osip_strncpy (p18, p1 + 1, (hp - p1 - 1));
+		p18 = malloc(i8);
+		i9 = i8-1;
+		p23 = p1 + 1;
+		strncpy (p18, p23, i9);
+		p18[i9] = 0;
+
 		if (0 != strcasecmp (p18, "SIP/2.0"))
 		{
-			MyPrintOutput("Wrong version number\n");
-			//fprintf(stdout,"Wrong version number\n");
+			MyMyPrintOutput("Wrong version number\n");
 		}
 
-		hp++;
-		if ((*hp) && ('\r' == hp[-1]) && ('\n' == hp[0]))
+		p20++;
+		if ((p20[0] != 0) && ('\r' == p20[-1]) && ('\n' == p20[0]))
 		{
-			hp++;
+			p20++;
 		}
 	}
-	MyPrintOutput(">> I'M OUT OF HERE !!!\n\n");
-	//fprintf(stdout,">> I'M OUT OF HERE !!!\n\n");
+	MyMyPrintOutput(">> I'M OUT OF HERE !!!\n\n");
 }
