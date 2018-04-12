d6dc86e6bcd26064136a96a385d9ba299d877aab
Martin Nowack <martin.nowack@gmail.com>
2018-04-12 13:32:37 +0100

    Fix python2 linking

--- .travis/z3.sh
+++ .travis/z3.sh
@@ -13,6 +13,12 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   # Should we install libz3-dbg too?
   sudo apt-get -y install libz3 libz3-dev
 elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+  set +e
+  brew install python@2
+  if [[ "X$?" != "X0" ]]; then
+     brew link --overwrite python@2
+  fi
+  set -e
   brew install z3
 else
   echo "Unhandled TRAVIS_OS_NAME \"${TRAVIS_OS_NAME}\""
