bf287fa76dc82b4fc6cf2fae2cbe8e6134ee2479
Martin Nowack <martin.nowack@gmail.com>
2018-05-08 11:19:32 +0100

    Fix include files

--- lib/Module/KModule.cpp
+++ lib/Module/KModule.cpp
@@ -41,7 +41,7 @@
 #include "llvm/Support/Path.h"
 #include "llvm/Transforms/Scalar.h"
 
-#include <llvm/Transforms/Utils/Cloning.h>
+#include "llvm/Transforms/Utils/Cloning.h"
 
 #include <sstream>
 
