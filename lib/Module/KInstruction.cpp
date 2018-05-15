0d569572bfa7b7897a10f9b20b9349c04330dc44
Cristian Cadar <c.cadar@imperial.ac.uk>
2018-05-15 21:22:31 +0100

    Simplified printFileLine by using std::to_string, and removed unneeded version that takes an argument a stream

--- lib/Module/KInstruction.cpp
+++ lib/Module/KInstruction.cpp
@@ -8,7 +8,6 @@
 //===----------------------------------------------------------------------===//
 
 #include "klee/Internal/Module/KInstruction.h"
-#include "llvm/Support/raw_ostream.h"
 #include <string>
 
 using namespace llvm;
@@ -20,15 +19,8 @@ KInstruction::~KInstruction() {
   delete[] operands;
 }
 
-void KInstruction::printFileLine(llvm::raw_ostream &debugFile) const {
-  if (info->file != "")
-    debugFile << info->file << ":" << info->line;
-  else debugFile << "[no debug info]";
-}
-
-std::string KInstruction::printFileLine() const {
-  std::string str;
-  llvm::raw_string_ostream oss(str);
-  printFileLine(oss);
-  return oss.str();
+std::string KInstruction::getSourceLocation() const {
+  if (!info->file.empty())
+    return info->file + ":" + std::to_string(info->line);
+  else return "[no debug info]";
 }
