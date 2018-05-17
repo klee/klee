f710bca8469d20bb88c777cb55839a5275308e36
Timotej Kapus <tk1713@ic.ac.uk>
2018-05-17 14:21:59 +0100

    Remove some comments

--- runtime/POSIX/sockets.c
+++ runtime/POSIX/sockets.c
@@ -868,23 +868,3 @@ int setsockopt(int sockfd, int level, int optname,
 {
   return 0;
 }
-//#undef htons
-//#undef htonl
-//#undef ntohs
-//#undef ntohl
-//
-//uint16_t htons(uint16_t v) {
-//      return (v >> 8) | (v << 8);
-//}
-//
-//uint32_t htonl(uint32_t v) {
-//      return htons(v >> 16) | (htons((uint16_t) v) << 16);
-//}
-//
-//uint16_t ntohs(uint16_t v) {
-//      return htons(v);
-//}
-//
-//uint32_t ntohl(uint32_t v) {
-//      return htonl(v);
-//}
