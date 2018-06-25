50bfcc66749c6c12d89537f5ea758413b2e929e7
Timotej Kapus <tk1713@ic.ac.uk>
2018-06-25 16:01:56 +0100

    Improve the test and make assertion failures, test failures

--- test/Feature/IsSymbolic.c
+++ test/Feature/IsSymbolic.c
@@ -1,22 +1,25 @@
 // RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out %t1.bc
+// RUN: %klee -exit-on-error --output-dir=%t.klee-out %t1.bc  &> %t1.log
 
 #include <assert.h>
+#include "klee/klee.h"
 
 int main() {
   int x, y, z = 0;
-  int array[5] = {1,1,1,1,1};
   klee_make_symbolic(&x, sizeof x, "x");
   klee_make_symbolic(&y, sizeof y, "y");
-  if (x) {
+  klee_assume(y >= 0 & y <= 4);
+
+  int array[5] = {1,1,x,1,1};
+  if (x == 1) {
     assert(klee_is_symbolic(y));
+    assert(!klee_is_symbolic(array[y]));
   } else {
     assert(!klee_is_symbolic(z));
+    assert(klee_is_symbolic(array[y]));
   }
 
-  klee_assume(y >= 0 & y <= 4);
-  assert(!klee_is_symbolic(array[y]));
 
   return 0;
 }
