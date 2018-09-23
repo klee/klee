f05935dc16bda1748d02a71cb0278fa4ec03c12b
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2018-09-23 03:12:16 +0200

    Fix a crash when the last running state is terminated during merging

---
+++ test/Merging/state_termination.c
@@ -0,0 +1,21 @@
+// RUN: %llvmgcc -emit-llvm -g -c -o %t.bc %s
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --use-merge --debug-log-merge --search=dfs  %t.bc 
+
+#include <klee/klee.h>
+
+int main(int argc, char** args){
+
+  int x;
+
+  char str[5];
+  klee_make_symbolic(str, sizeof(str), "str");
+  char *s = str;
+
+  klee_open_merge();
+  while(*s != 's')
+      s++;
+  klee_close_merge();
+
+  return 0;
+}
