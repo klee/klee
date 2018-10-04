6f95d9be4fd0f8179de6eaba3dfb55c654147915
Martin Nowack <m.nowack@imperial.ac.uk>
2018-10-04 12:57:17 +0000

    Workaround for flaky coverage
    Merge unittest coverage results and system tests coverage results
    into one coverage report.

--- scripts/build/run-coverage.sh
+++ scripts/build/run-coverage.sh
@@ -14,11 +14,12 @@ if [[ "a$COVERAGE" != "a1" ]]; then
 fi
 
 function upload_coverage() {
-  tags=$1
-  codecov_suffix=(${tags// /})
+  file=$1
+  tags=$2
   ci_env=$(bash <(curl -s https://codecov.io/env))
-  docker run ${ci_env} -ti ${REPOSITORY}/klee:${LLVM_VERSION_SHORT}${LLVM_SUFFIX}${SOLVER_SUFFIX}${DEPS_SUFFIX} /bin/bash -c "cd /home/klee/klee_src && bash <(curl -s https://codecov.io/bash) -X gcov -y /home/klee/klee_src/.codecov.yml -f /home/klee/klee_build/coverage_all.info.${codecov_suffix} -F $tags"
+  docker run ${ci_env} -ti ${REPOSITORY}/klee:${LLVM_VERSION_SHORT}${LLVM_SUFFIX}${SOLVER_SUFFIX}${DEPS_SUFFIX} /bin/bash -c "cd /home/klee/klee_src && bash <(curl -s https://codecov.io/bash) -X gcov -y /home/klee/klee_src/.codecov.yml -f /home/klee/klee_build/coverage_all.info.${file} -F $tags"
 }
 
-upload_coverage systemtests
-upload_coverage unittests
+# FIXME Enable separated coverage tags again
+upload_coverage systemtests systemtests_unittests
+upload_coverage unittests systemtests_unittests
