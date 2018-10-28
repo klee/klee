46463adc26e29075c4cf87cc8f51d5a191929938
Julian Büning <julian.buening@rwth-aachen.de>
2018-10-28 16:11:18 +0100

    fix Executor::initializeGlobals for aliases pointing to another alias

--- test/Feature/Alias.c
+++ test/Feature/Alias.c
@@ -10,6 +10,10 @@
 int b = 52;
 extern int a __attribute__((alias("b")));
 
+// alias for alias
+// NOTE: this does not have to be before foo is known
+extern int foo2() __attribute__((alias("foo")));
+
 // alias for function
 int __foo() { return 52; }
 extern int foo() __attribute__((alias("__foo")));
@@ -19,6 +23,7 @@ int *c = &a;
 int main() {
   assert(a == 52);
   assert(foo() == 52);
+  assert(foo2() == 52);
   assert(*c == 52);
 
   return 0;
