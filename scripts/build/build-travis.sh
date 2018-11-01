f7f976bda498c3b81016c834bb83d5d73f440e85
Frank Busse <bb0xfb@gmail.com>
2018-11-01 14:17:24 +0000

    Travis OSX: install Python 3.x

--- scripts/build/build-travis.sh
+++ scripts/build/build-travis.sh
@@ -7,10 +7,14 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew install ccache
   set +e
-  brew install python@2
-  if [[ "X$?" != "X0" ]]; then
-     brew link --overwrite python@2
-  fi
+  brew upgrade python               # upgrade to Python 3
+  brew install python@2             # install Python 2 (OSX version outdated + pip missing)
+  brew link --overwrite python@2    # keg-only => link manually
+  rm -f /usr/local/bin/{python,pip} # re-link python/pip to Python 2
+  py2path=$(find /usr/local/Cellar/python@2/ -name "python" -type f | head -n 1)
+  pip2path=$(find /usr/local/Cellar/python@2/ -name "pip" -type f | head -n 1)
+  ln -s "${py2path}" /usr/local/bin/python
+  ln -s "${pip2path}" /usr/local/bin/pip
   set -e
   pip install lit==0.6.0
   export PATH="/usr/local/opt/ccache/libexec:$PATH"
