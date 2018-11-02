e54647babcdc9cc5b3ccd4e079cea480322acb42
Cristian Cadar <c.cadar@imperial.ac.uk>
2018-11-02 14:49:03 +0000

    Added test for the case where external calls are encountered but disallowed

---
+++ test/Feature/NoExternalCallsAllowed.c
@@ -0,0 +1,13 @@
+// RUN: %llvmgcc %s -emit-llvm %O0opt -c -g -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --external-calls=none %t1.bc 2>&1 | FileCheck %s
+// RUN: test %t.klee-out/test000001.user.err
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  // CHECK: Disallowed call to external function: abs
+  int x = abs(argc);
+  printf("%d\n", argc);
+  return 0;
+}
