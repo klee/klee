6bc6c42f29a63491e89b21aebe4a2f5f304f2c7b
Frank Busse <bb0xfb@gmail.com>
2019-03-07 13:06:27 +0000

    tests: rename xxclang to clangxx

--- test/Feature/LargeReturnTypes.cpp
+++ test/Feature/LargeReturnTypes.cpp
@@ -1,5 +1,5 @@
 // REQUIRES: not-darwin
-// RUN: %xxclang -g -fno-exceptions -emit-llvm %O0opt -c -o %t.bc %s
+// RUN: %clangxx -g -fno-exceptions -emit-llvm %O0opt -c -o %t.bc %s
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --libc=klee --no-output --exit-on-error %t.bc > %t.log
 
