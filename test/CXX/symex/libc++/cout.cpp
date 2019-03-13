58fc7329dd810e3eca3ea2885a8c661bfb9fa0f9
Martin Nowack <m.nowack@imperial.ac.uk>
2019-03-13 13:28:48 +0000

    Fix libc++ testcases
    * remove wrapper script invocation and script
    * add build instruction to test cases
    * added additional checks
    * add check to avoid execution of tests if KLEE is not compiled with
    libc++

--- test/CXX/symex/libc++/cout.cpp
+++ test/CXX/symex/libc++/cout.cpp
@@ -1,4 +1,10 @@
-// RUN: sh %S/compile_with_libcxx.sh "%llvmgxx" "%s" "%S" "%t" "%klee" "%libcxx_include"
+// REQUIRES: libcxx
+// RUN: %clangxx %s -emit-llvm %O0opt -c -std=c++11 -I "%libcxx_include" -g -nostdinc++ -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --exit-on-error --libc=uclibc --libcxx %t1.bc 2>&1 | FileCheck %s
+
+// CHECK-DAG: cout
+// CHECK-DAG: cerr
 
 #include <iostream>
 
--- test/CXX/symex/libc++/vector.cpp
+++ test/CXX/symex/libc++/vector.cpp
@@ -1,5 +1,8 @@
-// RUN: sh %S/compile_with_libcxx.sh "%llvmgxx" "%s" "%S" "%t" "%klee" "%libcxx_include" | FileCheck %s
-
+// REQUIRES: libcxx
+// RUN: %clangxx %s -emit-llvm %O0opt -c -std=c++11 -I "%libcxx_include" -g -nostdinc++ -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --exit-on-error --libc=uclibc --libcxx %t1.bc 2>&1 | FileCheck %s
+//
 // CHECK: KLEE: done: completed paths = 1
 
 #include "klee/klee.h"
