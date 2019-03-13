a3774e15ea6c3d1c6b0aed17d5190d50910c6443
Martin Nowack <m.nowack@imperial.ac.uk>
2019-03-13 13:28:48 +0000

    Fix libc++ testcases
    * remove wrapper script invocation and script
    * add build instruction to test cases
    * added additional checks
    * add check to avoid execution of tests if KLEE is not compiled with
    libc++

--- test/CXX/symex/libc++/atexit.cpp
+++ test/CXX/symex/libc++/atexit.cpp
@@ -1,4 +1,7 @@
-// RUN: sh %S/compile_with_libcxx.sh "%llvmgxx" "%s" "%S" "%t" "%klee" "%libcxx_include" | FileCheck %s
+// REQUIRES: libcxx
+// RUN: %clangxx %s -emit-llvm %O0opt -c -std=c++11 -I "%libcxx_include" -g -nostdinc++ -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --exit-on-error --libc=uclibc --libcxx %t1.bc | FileCheck %s
 //
 // CHECK: Returning from main
 // CHECK: atexit_1
