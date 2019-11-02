02bd78ec39f04d7617e3b0f249e5d731b9a18666
Cristian Cadar <c.cadar@imperial.ac.uk>
2019-11-02 23:17:28 +0000

    Most libc++ tests require uclibc; add missing REQUIRES statements or remove dependency.

--- test/CXX/symex/libc++/cout_sym.cpp
+++ test/CXX/symex/libc++/cout_sym.cpp
@@ -1,4 +1,5 @@
 // REQUIRES: libcxx
+// REQUIRES: uclibc
 // RUN: %clangxx %s -emit-llvm %O0opt -c -std=c++11 -I "%libcxx_include" -g -nostdinc++ -o %t1.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --exit-on-error --libc=uclibc --libcxx %t1.bc | FileCheck %s
