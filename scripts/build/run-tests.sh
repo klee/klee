2c085b56c866fd7b74e78551a6705407887a1e16
Martin Nowack <m.nowack@imperial.ac.uk>
2020-01-23 15:31:27 +0000

    Do not wait if SANITIZER_BUILD is empty

--- scripts/build/run-tests.sh
+++ scripts/build/run-tests.sh
@@ -58,8 +58,10 @@ run_tests() {
     coverage_setup "${build_dir}"
   fi
 
-  if [[ -n "${SANITIZER_BUILD+x}" ]]; then
-    for num in {1..10}; do sleep 120; echo 'Keep Travis alive'; done &
+  if [[ -n "${SANITIZER_BUILD+x}" ]]; then # Check for existance of variable
+    if [[ -n "${SANITIZER_BUILD}" ]]; then # Check for variable not being empty string
+      for num in {1..10}; do sleep 120; echo 'Keep Travis alive'; done &
+    fi
   fi
 
   make systemtests || return 1
