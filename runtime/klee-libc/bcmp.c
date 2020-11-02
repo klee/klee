bef9f39d033ea5c9600f39af7cfd213e01aab4c1
Alastair Reid <adreid@google.com>
2020-11-02 16:47:51 +0000

    fix: bcmp with n==0
    This was executing the loop when n==0 leading to an out of bound pointer
    error.
    
    Found while verifying Rust code that compares strings.

--- runtime/klee-libc/bcmp.c
+++ runtime/klee-libc/bcmp.c
@@ -11,7 +11,7 @@
 
 int bcmp(const void *s1, const void *s2, size_t n) {
   const unsigned char *p1 = s1, *p2 = s2;
-  while (--n != 0) {
+  while (n-- != 0) {
     if (*p1++ != *p2++)
       return 1;
   }
