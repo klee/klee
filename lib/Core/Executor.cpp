e452ddded446a9bfd799f455ff278d65125954c3
Cristian Cadar <c.cadar@imperial.ac.uk>
2020-12-22 19:52:12 +0000

    Added -max-static-pct-check-delay to replace the hardcoded delay after which the MaxStatic*Pct checks are performed.

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -366,6 +366,12 @@ cl::opt<double> MaxStaticCPSolvePct(
              "instructions (default=1.0 (always))"),
     cl::cat(TerminationCat));
 
+cl::opt<std::string> MaxStaticPctCheckDelay(
+    "max-static-pct-check-delay",
+    cl::desc("Time after which the --max-static-*-pct checks are enforced (default=60s)"),
+    cl::init("60s"),
+    cl::cat(TerminationCat));
+
 cl::opt<std::string> TimerInterval(
     "timer-interval",
     cl::desc("Minimum interval to check timers. "
@@ -510,6 +516,8 @@ Executor::Executor(LLVMContext &ctx, const InterpreterOptions &opts,
 
   this->solver = new TimingSolver(solver, EqualitySubstitution);
   memory = new MemoryManager(&arrayCache);
+
+  maxStaticPctCheckDelay = time::Span{MaxStaticPctCheckDelay};
 
   initializeSearchOptions();
 
@@ -998,7 +1006,9 @@ ref<Expr> Executor::maxStaticPctChecks(ExecutionState &current,
       MaxStaticCPForkPct == 1. && MaxStaticCPSolvePct == 1.)
     return condition;
 
-  if (statsTracker->elapsed() <= time::seconds(60))
+  // these checks are performed only after MaxStaticPctCheckDelay time has
+  // passed since execution started
+  if (statsTracker->elapsed() <= maxStaticPctCheckDelay)
     return condition;
 
   StatisticManager &sm = *theStatisticManager;
