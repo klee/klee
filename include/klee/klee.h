6ef22d29eca39ee6202f392dabdb637d94c2ebb4
Sumit Lahiri <lahiri.phdworks@gmail.com>
2020-12-29 11:02:19 +0530

    Resolved PR

--- include/klee/klee.h
+++ include/klee/klee.h
@@ -17,6 +17,27 @@
 extern "C" {
 #endif
 
+/* klee_dump_kquery_state() get KQuery Dump for current State */
+void klee_dump_kquery_state();
+
+/* klee_dump_state_stack() get the state stack dump*/
+void klee_dump_state_stack();
+
+/* klee_dump_symbolic_details() get the known Symbolics of an expression */
+void klee_dump_symbolic_details(void *addr, const char *name);
+
+/* klee_make_pse_symbolic - Make the contents of the object pointer to by \arg
+ * addr as a probabilistic symbolic variable.
+ *
+ * \arg addr - The start of the object.
+ * \arg nbytes - The number of bytes to make symbolic; currently this *must*
+ * be the entire contents of the object.
+ * \arg name - A name used for identifying the object in messages, output
+ * files, etc. If NULL, object is called "unnamed".
+ */
+void klee_make_pse_symbolic(void *addr, size_t nbytes, const char *name,
+                            float *distribution, float *probabilities);
+
 /* Add an accesible memory object at a user specified location. It
  * is the users responsibility to make sure that these memory
  * objects do not overlap. These memory objects will also
