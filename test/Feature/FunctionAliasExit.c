2fe27adb39db5b48d5c246fe92ad41c51ae7d7f7
Aleksandr Misonizhnik <misonijnik@gmail.com>
2021-02-10 21:17:44 +0300

    Fix tests

--- test/Feature/FunctionAliasExit.c
+++ test/Feature/FunctionAliasExit.c
@@ -1,6 +1,6 @@
 // RUN: %clang %s -emit-llvm %O0opt -c -o %t.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out -function-alias=exit:end %t.bc 2>&1 | FileCheck %s
+// RUN: %klee --output-dir=%t.klee-out --search=bfs -function-alias=exit:end %t.bc 2>&1 | FileCheck %s
 
 #include <stdio.h>
 #include <stdlib.h>
