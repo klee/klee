8635898b2759b3ce55de07176fa6b77031128773
Julian Büning <julian.buening@rwth-aachen.de>
2022-01-04 20:32:30 +0100

    fix CMake: use own TestMain instead of gtest_main
    Before, we would use gtest_main as provided by either LLVM or Google
    Test itself; the TestMain.cpp was ignored (contrary to its source code
    comment). In newer versions of Google Test (1.8.1+), gtest_main uses
    `__FILE__` for its "Running main() from" line, but llvm-lit (which we
    use to invoke unit tests) currently matches exactly "Running main()
    from gtest_main.cc" for determining whether to skip this line. This
    results in spurious "tests" that will be shown as unresolved.

--- unittests/TestMain.cpp
+++ unittests/TestMain.cpp
@@ -1,6 +1,6 @@
 //===--- unittests/TestMain.cpp - unittest driver -------------------------===//
 //
-//                     The LLVM Compiler Infrastructure
+//                     The KLEE Symbolic Virtual Machine
 //
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
@@ -9,11 +9,6 @@
 
 #include "gtest/gtest.h"
 
-// WARNING: If LLVM's gtest_main target is reused
-//          or is built from LLVM's source tree,
-//          this file is ignored. Instead, LLVM's
-//          utils/unittest/UnitTestMain/TestMain.cpp
-//          is used.
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
