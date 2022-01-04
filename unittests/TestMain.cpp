62e27ff8cad97c12f3051a5fdcf8cd4aade96894
Julian Büning <julian.buening@rwth-aachen.de>
2022-01-04 20:39:47 +0100

    unittests/TestMain: show stack trace on error

--- unittests/TestMain.cpp
+++ unittests/TestMain.cpp
@@ -7,10 +7,21 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "klee/Config/Version.h"
+
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Signals.h"
+
 #include "gtest/gtest.h"
 
-
 int main(int argc, char **argv) {
+
+#if LLVM_VERSION_CODE >= LLVM_VERSION(3, 9)
+  llvm::sys::PrintStackTraceOnErrorSignal(argv[0], true);
+#else
+  llvm::sys::PrintStackTraceOnErrorSignal(true);
+#endif
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
