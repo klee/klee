92db9adf34937e44a123a5c2b9a99145346aa679
Aleksandr Misonizhnik <misonijnik@gmail.com>
2022-02-18 21:02:51 +0300

    [test] Enable MSan [test] Brackets added [test] Disabling ubsan_vla_bound.c

--- test/Feature/ubsan/ubsan_vla_bound.c
+++ test/Feature/ubsan/ubsan_vla_bound.c
@@ -1,3 +1,5 @@
+// REQUIRES: not-msan
+// Disabling msan because it crash during posix_memalign call on CI
 // RUN: %clang %s -fsanitize=vla-bound -emit-llvm -g %O0opt -c -o %t.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out %t.bc 2>&1 | FileCheck %s
@@ -6,7 +8,7 @@
 
 int main() {
   int x;
-  klee_make_symbolic(&x, sizeof x, "x");
+  klee_make_symbolic(&x, sizeof(x), "x");
   // CHECK: ubsan_vla_bound.c:[[@LINE+1]]: variable length array bound evaluates to non-positive value
   int arr[x];
   volatile int result = arr[0];
