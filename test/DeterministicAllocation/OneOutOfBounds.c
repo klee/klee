6bd17cafdc62f7f01e1c1c9d8613f020c0d16ec6
Daniel Schemmel <daniel@schemmel.net>
2022-10-13 14:25:43 +0100

    Add some system tests for KDAlloc

---
+++ test/DeterministicAllocation/OneOutOfBounds.c
@@ -0,0 +1,12 @@
+// RUN: %clang %s -g -emit-llvm %O0opt -c -o %t.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --kdalloc %t.bc 2>&1 | FileCheck %s
+// RUN: test -f %t.klee-out/test000001.ptr.err
+
+int main() {
+  int *x = malloc(sizeof(int));
+  // CHECK: OneOutOfBounds.c:[[@LINE+1]]: memory error: out of bound pointer
+  x[1] = 1;
+  free(x);
+  return 0;
+}
