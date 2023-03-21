94376216f09802dd1d402b317dd703071e9a7099
Cristian Cadar <c.cadar@imperial.ac.uk>
2023-03-21 17:19:14 +0000

    Added more test cases for --entry-point. EntryPointMissing is currently expected to fail.

--- test/Feature/EntryPoint.c
+++ test/Feature/EntryPoint.c
@@ -3,6 +3,10 @@
 // RUN: %klee --output-dir=%t.klee-out --entry-point=other_main %t.bc | FileCheck -check-prefix=CHECK-OTHER_MAIN %s
 
 // RUN: rm -rf %t.klee-out
+// RUN: %clang -emit-llvm -g -c -DMAIN %s -o %t.bc
+// RUN: %klee --output-dir=%t.klee-out --entry-point=other_main %t.bc | FileCheck -check-prefix=CHECK-OTHER_MAIN %s
+
+// RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --entry-point="" %t.bc 2>%t.stderr.log || echo "Exit status must be 0"
 // RUN: FileCheck -check-prefix=CHECK-EMPTY --input-file=%t.stderr.log %s
 
@@ -13,5 +17,11 @@ int other_main() {
   // CHECK-OTHER_MAIN: Hello World
   return 0;
 }
+
+#ifdef MAIN
+int main() {
+  return 0;
+}
+#endif
 
 // CHECK-EMPTY: KLEE: ERROR: entry-point cannot be empty
