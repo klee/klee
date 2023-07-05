8ad9f2aaf4e9e51c3f8b5e24aec56dae4e49e983
Daniel Schemmel <daniel@schemmel.net>
2023-07-05 14:11:42 +0000

    remove timings from kdalloc tests

--- unittests/KDAlloc/stacktest.cpp
+++ unittests/KDAlloc/stacktest.cpp
@@ -15,7 +15,6 @@
 #endif
 
 #include <cassert>
-#include <chrono>
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
@@ -111,18 +110,8 @@ public:
 } // namespace
 
 void stack_test() {
-  auto start = std::chrono::steady_clock::now();
-
   RandomTest tester;
   tester.run(10'000'000);
-
-  auto stop = std::chrono::steady_clock::now();
-  std::cout << std::dec
-            << std::chrono::duration_cast<std::chrono::milliseconds>(stop -
-                                                                     start)
-                   .count()
-            << " ms\n";
-  std::cout << "\n";
 
   std::cout << "Allocations: " << tester.allocation_count << "\n";
   std::cout << "Deallocations: " << tester.deallocation_count << "\n";
