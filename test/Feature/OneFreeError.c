2e393db175cd55662f369d15905ce4eb44b154a9
Cristian Cadar <c.cadar@imperial.ac.uk>
2023-08-18 15:32:52 +0100

    Changed use-after-free and double-free tests to expect KDAlloc, plus some small improvements.

--- test/Feature/OneFreeError.c
+++ test/Feature/OneFreeError.c
@@ -1,12 +1,14 @@
 // RUN: %clang %s -g -emit-llvm %O0opt -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out %t1.bc 2>&1 | FileCheck %s
+// RUN: %klee --output-dir=%t.klee-out --kdalloc %t1.bc 2>&1 | FileCheck %s
 // RUN: test -f %t.klee-out/test000001.ptr.err
 
+#include <stdlib.h>
+
 int main() {
-  int *x = malloc(4);
+  int *x = malloc(sizeof(*x));
   free(x);
-  // CHECK: OneFreeError.c:[[@LINE+1]]: memory error: out of bound pointer
+  // CHECK: OneFreeError.c:[[@LINE+1]]: memory error: use after free
   x[0] = 1;
   return 0;
 }
--- test/regression/2007-10-11-illegal-access-after-free-and-branch.c
+++ test/regression/2007-10-11-illegal-access-after-free-and-branch.c
@@ -1,21 +1,21 @@
 // RUN: %clang %s -emit-llvm -g -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --optimize %t1.bc 2>&1 | FileCheck %s
+// RUN: %klee --output-dir=%t.klee-out --kdalloc --optimize %t1.bc 2>&1 | FileCheck %s
 // RUN: test -f %t.klee-out/test000001.ptr.err
 
+#include "klee/klee.h"
+
 #include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
 
 int main(int argc, char **argv) {
   unsigned char *buf = malloc(3);
   klee_make_symbolic(buf, 3, "buf");
-  if (buf[0]>4) klee_silent_exit(0);
+  if (buf[0] > 4)
+    klee_silent_exit(0);
   unsigned char x = buf[1];
   free(buf);
-  if (x)
-  {
-    // CHECK: 2007-10-11-illegal-access-after-free-and-branch.c:19: memory error: out of bound pointer
+  if (x) {
+    // CHECK: 2007-10-11-illegal-access-after-free-and-branch.c:[[@LINE+1]]: memory error: use after free
     return buf[2];
   }
   klee_silent_exit(0);
