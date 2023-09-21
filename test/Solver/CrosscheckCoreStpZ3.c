026da463e728bf4f20d942aee85f10cb6236447b
Aleksandr Misonizhnik <misonijnik@gmail.com>
2023-09-21 18:58:00 +0400

    [fix] Make the tests independent of default search strategies

--- test/Solver/CrosscheckCoreStpZ3.c
+++ test/Solver/CrosscheckCoreStpZ3.c
@@ -2,9 +2,9 @@
 // REQUIRES: z3
 // RUN: %clang %s -emit-llvm %O0opt -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --solver-backend=stp --use-forked-solver=false --debug-crosscheck-core-solver=z3 %t1.bc 2>&1 | FileCheck %s
+// RUN: %klee --output-dir=%t.klee-out --solver-backend=stp --use-forked-solver=false --debug-crosscheck-core-solver=z3 --use-guided-search=none %t1.bc 2>&1 | FileCheck %s
 
 #include "ExerciseSolver.c.inc"
 
-// CHECK: KLEE: done: completed paths = 9
-// CHECK: KLEE: done: partially completed paths = 5
+// CHECK: KLEE: done: completed paths = 18
+// CHECK: KLEE: done: partially completed paths = 0
--- test/Solver/ValidatingSolver.c
+++ test/Solver/ValidatingSolver.c
@@ -1,8 +1,8 @@
 // RUN: %clang %s -emit-llvm %O0opt -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --debug-validate-solver --debug-assignment-validating-solver %t1.bc 2>&1 | FileCheck %s
+// RUN: %klee --output-dir=%t.klee-out --search=bfs --debug-validate-solver --debug-assignment-validating-solver --use-guided-search=none %t1.bc 2>&1 | FileCheck %s
 
 #include "ExerciseSolver.c.inc"
 
-// CHECK: KLEE: done: completed paths = 9
-// CHECK: KLEE: done: partially completed paths = 5
+// CHECK: KLEE: done: completed paths = 18
+// CHECK: KLEE: done: partially completed paths = 0
