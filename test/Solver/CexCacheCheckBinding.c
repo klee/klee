4ac916348ee90a542c2285f3a3b913e9e23fa151
Aleksandr Misonizhnik <misonijnik@gmail.com>
2023-10-05 23:31:23 +0400

    [chore] Add a test

---
+++ test/Solver/CexCacheCheckBinding.c
@@ -0,0 +1,8 @@
+// RUN: %clang %s -emit-llvm %O0opt -c -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --debug-cex-cache-check-binding --search=bfs --use-guided-search=none --use-cex-cache=false %t1.bc 2>&1 | FileCheck %s
+
+#include "ExerciseSolver.c.inc"
+
+// CHECK: KLEE: done: completed paths = 18
+// CHECK: KLEE: done: partially completed paths = 0
