b4fbe6d7fa1deb7e32e0b24c0755f24e0af8bd2c
Aleksandr Misonizhnik <misonijnik@gmail.com>
2023-10-25 02:03:49 +0400

    [fix] Update test

--- test/Feature/SymbolicSizes/IntArray.c
+++ test/Feature/SymbolicSizes/IntArray.c
@@ -1,5 +1,5 @@
-// REQUIRES: not-darwin
-// Disabling darwin because it overflows stack on CI
+// REQUIRES: not-darwin, not-metasmt
+// Disabling darwin and metasmt because test has flaky behaviour on CI
 // RUN: %clang %s -g -emit-llvm %O0opt -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --use-sym-size-alloc --use-sym-size-li --min-number-elements-li=1 --skip-not-lazy-initialized %t1.bc 2>&1 | FileCheck %s
