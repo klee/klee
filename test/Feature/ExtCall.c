4f3b217647a114f564561184d6cee3ba7f432870
Cristian Cadar <c.cadar@imperial.ac.uk>
2024-02-17 14:50:41 +0000

    Two test cases exercising two policies for calling external calls with symbolic arguments.  One of them is currently expected to fail.

---
+++ test/Feature/ExtCall.c
@@ -0,0 +1,24 @@
+// XFAIL: *
+// This test checks that symbolic arguments to a function call are correctly concretized
+// RUN: %clang %s -emit-llvm %O0opt -g -c -o %t.bc
+
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --external-calls=all --exit-on-error %t.bc 2>&1 | FileCheck %s
+
+#include "klee/klee.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int x;
+  klee_make_symbolic(&x, sizeof(x), "x");
+  klee_assume(x >= 0);
+
+  int y = abs(x);
+  printf("y = %d\n", y);
+  // CHECK: calling external: abs((ReadLSB w32 0 x))
+
+  assert(x == y);
+}
