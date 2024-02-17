85858749a65f599b88098662de332b6878e6af4c
Cristian Cadar <c.cadar@imperial.ac.uk>
2024-02-17 14:50:41 +0000

    Two test cases exercising two policies for calling external calls with symbolic arguments.  One of them is currently expected to fail.

---
+++ test/Feature/ExtCallOverapprox.c
@@ -0,0 +1,26 @@
+// This test checks that under using the under-approximate external call policy, the symbolic arguments are left unconstrained by the external call
+
+// RUN: %clang %s -emit-llvm %O0opt -g -c -o %t.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --external-calls=all %t.bc 2>&1 | FileCheck %s
+
+#include "klee/klee.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int x;
+  klee_make_symbolic(&x, sizeof(x), "x");
+
+  printf("%d\n", x);
+  // CHECK: calling external: printf
+  if (x > 0) {
+    printf("Yes\n");
+    // CHECK-DAG: Yes
+  } else {
+    printf("No\n");
+    // CHECK-DAG: No
+  }
+}
