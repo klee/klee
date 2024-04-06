9b3b71a471f2cd822eec19cf74e29ee7114b7ff5
lunal1ght <alltakethat228@gmail.com>
2024-04-06 23:58:54 +0300

    no words 8

--- scripts/build/run-tests.sh
+++ scripts/build/run-tests.sh
@@ -10,8 +10,7 @@ coverage_setup() {
   lcov -q --directory "${build_dir}" --no-external --zerocounters
   # Create a baseline by capturing any file used for compilation, no execution yet
   lcov -q --rc lcov_branch_coverage=1 --directory "${build_dir}" --base-directory="${KLEE_SRC}" --no-external --capture --initial --output-file coverage_base.info
-  lcov -q --rc lcov_branch_coverage=1 --remove coverage_base.info 'test/*' --output-file coverage_base.info
-  lcov -q --rc lcov_branch_coverage=1 --remove coverage_base.info 'unittests/*' --output-file coverage_base.info
+  lcov -q --rc lcov_branch_coverage=1 --remove coverage_base.info 'test/unittestsmark/*' --output-file coverage_base.info
 }
 
 coverage_update() {
@@ -22,8 +21,7 @@ coverage_update() {
   # (NOTE: "--rc lcov_branch_coverage=1" needs to be added in all calls, otherwise branch coverage gets dropped)
   lcov -q --rc lcov_branch_coverage=1 --directory "${build_dir}" --base-directory="${KLEE_SRC}" --no-external --capture --output-file coverage.info
   # Exclude uninteresting coverage goals (LLVM, googletest, and KLEE system and unit tests)
-  lcov -q --rc lcov_branch_coverage=1 --remove coverage.info 'test/*' --output-file coverage.info
-  lcov -q --rc lcov_branch_coverage=1 --remove coverage.info 'unittests/*' --output-file coverage.info
+  lcov -q --rc lcov_branch_coverage=1 --remove coverage.info 'test/unittestsmark/*' --output-file coverage.info
   # Combine baseline and measured coverage
   lcov -q --rc lcov_branch_coverage=1 -a coverage_base.info -a coverage.info -o coverage_all.info."${codecov_suffix}"
   # Debug info
@@ -50,11 +48,11 @@ run_tests() {
   if [ "${COVERAGE}" -eq 1 ]; then
     coverage_setup "${build_dir}"
   fi
-  make unittests
+  make test/unittestsmark
   
   # Generate and upload coverage if COVERAGE is set
   if [ "${COVERAGE}" -eq 1 ]; then
-    coverage_update "${build_dir}" "unittests"
+    coverage_update "${build_dir}" "test/unittestsmark"
   fi
 
   ###############################################################################
@@ -64,7 +62,7 @@ run_tests() {
     coverage_setup "${build_dir}"
   fi
 
-  make systemtests || return 1
+  make test/unittestsmark || return 1
   
   # If metaSMT is the only solver, then rerun lit tests with non-default metaSMT backends
   if [ "X${SOLVERS}" == "XmetaSMT" ]; then
@@ -73,14 +71,14 @@ run_tests() {
     available_metasmt_backends="btor stp z3 yices2 cvc4"
     for backend in $available_metasmt_backends; do
       if [ "X${METASMT_DEFAULT}" != "X$backend" ]; then
-        lit -v --param klee_opts=-metasmt-backend="$backend" --param kleaver_opts=-metasmt-backend="$backend" test/
+        lit -v --param klee_opts=-metasmt-backend="$backend" --param kleaver_opts=-metasmt-backend="$backend" test/unittestsmark/
       fi
     done
   fi
   
   # Generate and upload coverage if COVERAGE is set
   if [ "${COVERAGE}" -eq 1 ]; then
-    coverage_update "${build_dir}" "systemtests"
+    coverage_update "${build_dir}" "test/unittestsmark"
   fi
 }
 
@@ -148,12 +146,6 @@ main() {
   fi
 
   run_tests "${directory}"
-  
-  # FIXME Enable separated coverage tags again
-  if [[ "${UPLOAD_COVERAGE}" -eq 1 ]]; then
-    upload_coverage systemtests systemtests_unittests
-    upload_coverage unittests systemtests_unittests
-  fi
- }
+
 
 main "$@"
