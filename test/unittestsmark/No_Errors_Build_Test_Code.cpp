b3f358e2ad8eaa6cb8eff53d07a7aaa10047361f
lunal1ght <alltakethat228@gmail.com>
2024-04-06 23:16:45 +0300

    any word 2

---
+++ test/unittestsmark/No_Errors_Build_Test_Code.cpp
@@ -0,0 +1,15 @@
+int get_sign(int x) {
+  if (x == 0)
+    return 0;
+
+  if (x < 0)
+    return -1;
+  else
+    return 1;
+}
+
+int main() {
+  int a;
+  klee_make_symbolic(&a, sizeof(a), "a");
+  return get_sign(a);
+}
