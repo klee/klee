8c1e88966c1783b6d22636564431cf6285f9d09b
lunal1ght <alltakethat228@gmail.com>
2024-04-07 13:07:33 +0300

    no words 58

--- test/deadlockmark/deadlock.cpp
+++ test/deadlockmark/deadlock.cpp
@@ -1,3 +1,7 @@
+// RUN: %clangxx %s -emit-llvm %O0opt -c -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --write-no-tests --exit-on-error --external-calls=none %t1.bc
+
 #include <iostream>
 #include <thread>
 #include <mutex>
