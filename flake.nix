c7e1e10350df0345564a54678e36b73582c4187b
jiezhuzzz <jiezzz@duck.com>
2025-09-08 01:03:08 +0000

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,44 @@
 {
   description = "A Nix-flake-based C/C++ development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
 
-  outputs = inputs:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import inputs.nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell.override
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+
+        devShells.default =
+          pkgs.mkShell.override
+          {stdenv = pkgs.llvmPackages_13.stdenv;}
           {
-            stdenv = pkgs.llvmPackages_13.stdenv;
-          }
-          {
-            packages = with pkgs; [
-              cmake
-              z3
-              gperftools
-              sqlite
-              ninja
-              cppcheck
-              lit
-            ] ++ (with pkgs.llvmPackages_13; [
-              libllvm
-              libcxx
-              clang-tools
-            ]);
+            packages = with pkgs;
+              [
+                cmake
+                z3
+                gperftools
+                sqlite
+                ninja
+                cppcheck
+                lit
+              ]
+              ++ (with pkgs.llvmPackages_13; [
+                libllvm
+                libcxx
+                clang-tools
+              ]);
           };
-      });
+      };
     };
 }
