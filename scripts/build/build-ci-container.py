8b4fa4548e2118405a22bac8c7422f63821b131d
Cristian Cadar <c.cadar@imperial.ac.uk>
2026-06-24 13:51:14 +0100

    Include Coverage matrix as part of the build script for Docker images

--- scripts/build/build-ci-container.py
+++ scripts/build/build-ci-container.py
@@ -11,48 +11,54 @@ with open(os.path.join(abs_path,"../../.github/workflows/build.yaml"), 'r') as s
     try:
         ci_config = yaml.safe_load(stream)
         global_env = ci_config['env']
-        for job in ci_config['jobs']['Linux']['strategy']['matrix']['include']:
-            if job['name'] in ["Docker", "macOS"]:
-                print("Skip: {}".format(job['name']))
-                continue
+        build_jobs = ('Linux', 'Coverage')
 
-            print("Building: {}".format(job['name']))
+        for job_name in build_jobs:
+            job_config = ci_config['jobs'][job_name]
+            job_env = job_config.get('env', {})
 
-            build_env = os.environ.copy()
+            for matrix_entry in job_config['strategy']['matrix']['include']:
+                print("Building: {} ({})".format(job_name, matrix_entry['name']))
 
-            # Copy current global build configurations
-            for k,v in global_env.items():
-              build_env[k] = str(v)
+                build_env = os.environ.copy()
 
-            # Override with job specific values
-            for k,v in job['env'].items():
-                build_env[k] = str(v)
+                # Copy current global build configurations
+                for k,v in global_env.items():
+                    build_env[k] = str(v)
 
-            cmd = [os.path.join(abs_path, 'build.sh'),
-                  'klee', # build KLEE and all its dependencies
-                  '--docker', # using docker containers
-                  '--push-docker-deps', # push containers if possible
-                  '--debug', # print debug output
-                  '--create-final-image', # assume KLEE is the final image
-                  ]
+                # Override with job-level values
+                for k,v in job_env.items():
+                    build_env[k] = str(v)
+
+                # Override with matrix-entry-specific values
+                for k,v in matrix_entry.get('env', {}).items():
+                    build_env[k] = str(v)
+
+                cmd = [os.path.join(abs_path, 'build.sh'),
+                      'klee', # build KLEE and all its dependencies
+                      '--docker', # using docker containers
+                      '--push-docker-deps', # push containers if possible
+                      '--debug', # print debug output
+                      '--create-final-image', # assume KLEE is the final image
+                      ]
 
 
-            process = subprocess.Popen(cmd, # Assume KLEE is the final image
-                            stdout=subprocess.PIPE,
-                            universal_newlines=True,
-                            env = build_env)
+                process = subprocess.Popen(cmd, # Assume KLEE is the final image
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True,
+                                env = build_env)
 
-            while True:
-                output = process.stdout.readline()
-                print(output.strip())
-                return_code = process.poll()
-                if return_code != None:
-                    break
-            if return_code != 0:
-                print('Building image failed: {}'.format(return_code))
-                for output in process.stdout.readlines():
+                while True:
+                    output = process.stdout.readline()
                     print(output.strip())
-                sys.exit(1)
+                    return_code = process.poll()
+                    if return_code != None:
+                        break
+                if return_code != 0:
+                    print('Building image failed: {}'.format(return_code))
+                    for output in process.stdout.readlines():
+                        print(output.strip())
+                    sys.exit(1)
 
     except yaml.YAMLError as exc:
         print(exc)
