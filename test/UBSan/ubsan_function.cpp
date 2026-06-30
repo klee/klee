bde2baa3f23e172beb6a1a3d6ba737e014abea85
Cristian Cadar <c.cadar@imperial.ac.uk>
2026-06-30 22:05:57 +0100

    Mark recent UBSan test as expected to fail

--- test/UBSan/ubsan_function.cpp
+++ test/UBSan/ubsan_function.cpp
@@ -1,4 +1,5 @@
 // REQUIRES: geq-llvm-17.0
+// XFAIL: *
 // RUN: %clangxx %s -fsanitize=function -emit-llvm -g %O0opt -c -o %t.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --emit-all-errors --ubsan-runtime %t.bc 2>&1 | FileCheck %s
