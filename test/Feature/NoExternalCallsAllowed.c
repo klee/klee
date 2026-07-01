88d2211eecefd8bd6ea62d6ddbb99da5221ec5c8
Cristian Cadar <c.cadar@imperial.ac.uk>
2026-07-01 21:17:57 +0100

    Recent versions of LLVM compile abs to llvm.abs.  Changed external call tests to use other external functions, in particular strlen() and div()

--- test/Feature/NoExternalCallsAllowed.c
+++ test/Feature/NoExternalCallsAllowed.c
@@ -1,13 +1,12 @@
 // RUN: %clang %s -emit-llvm %O0opt -c -g -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --external-calls=none %t1.bc 2>&1 | FileCheck %s
+// RUN: %klee --output-dir=%t.klee-out --external-calls=none %t1.bc abc def 2>&1 | FileCheck %s
 // RUN: test %t.klee-out/test000001.user.err
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
-  // CHECK: Disallowed call to external function: abs
-  int x = abs(argc);
-  printf("%d\n", argc);
-  return 0;
+  // CHECK: Disallowed call to external function: strlen
+  int len = strlen(argv[0]);
+  printf("%d\n", len);
 }
