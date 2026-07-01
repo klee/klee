2e8da30db4829506bcdc8b6c6e8b663a88ba5ce1
Cristian Cadar <c.cadar@imperial.ac.uk>
2026-07-01 21:17:57 +0100

    Recent versions of LLVM compile abs to llvm.abs.  Changed external call tests to use other external functions, in particular strlen() and div()

--- test/Feature/ExtCall.c
+++ test/Feature/ExtCall.c
@@ -2,22 +2,23 @@
 // RUN: %clang %s -emit-llvm %O0opt -g -c -o %t.bc
 
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --external-calls=all --exit-on-error %t.bc 2>&1 | FileCheck %s
+// RUN: %klee --output-dir=%t.klee-out --external-calls=all --exit-on-error %t.bc abc def 2>&1 | FileCheck %s
 
 #include "klee/klee.h"
 
 #include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char **argv) {
   int x;
   klee_make_symbolic(&x, sizeof(x), "x");
-  klee_assume(x >= 0);
+  // x is 0 or 1; argv[1] is "abc"; argv[2] is "def"
+  klee_assume(x >= 1 & x <= 2);
 
-  int y = abs(x);
-  printf("y = %d\n", y);
-  // CHECK: calling external: abs((ReadLSB w32 0 x))
+  int len = strlen(argv[x]);
+  printf("len = %d\n", len);
+  // CHECK: calling external: strlen
 
-  assert(x == y);
+  assert(len == 3);
 }
