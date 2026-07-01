48effca9a85aeedf95fcdf0a4dccdb7cbe979457
Cristian Cadar <c.cadar@imperial.ac.uk>
2026-07-01 21:17:57 +0100

    Recent versions of LLVM compile abs to llvm.abs.  Changed external call tests to use other external functions, in particular strlen() and div()

--- test/Feature/ExtCallWarnings.c
+++ test/Feature/ExtCallWarnings.c
@@ -1,20 +1,20 @@
 // RUN: %clang %s -emit-llvm %O0opt -g -c -o %t.bc
 
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --external-call-warnings=none %t.bc 2>&1 | FileCheck --check-prefix=CHECK-NONE %s
+// RUN: %klee --output-dir=%t.klee-out --external-call-warnings=none %t.bc abc defg 2>&1 | FileCheck --check-prefix=CHECK-NONE %s
 
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --external-call-warnings=once-per-function %t.bc 2>&1 | FileCheck --check-prefix=CHECK-ONCE %s
+// RUN: %klee --output-dir=%t.klee-out --external-call-warnings=once-per-function %t.bc abc defg 2>&1 | FileCheck --check-prefix=CHECK-ONCE %s
 
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --external-call-warnings=all %t.bc 2>&1 | FileCheck --check-prefix=CHECK-ALL %s
+// RUN: %klee --output-dir=%t.klee-out --external-call-warnings=all %t.bc abc defg 2>&1 | FileCheck --check-prefix=CHECK-ALL %s
 
 #include "klee/klee.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main() {
-  return abs(-5) + abs(6);
+int main(int argc, char** argv) {
+  return strlen(argv[1]) + strlen(argv[2]);
   // CHECK-NONE-NOT: calling external
 
   // CHECK-ONCE: calling external
